58c3885166a79742f44e895124c80cc950728a09
ricardox2x <ric.ace68@gmail.com>
2023-01-09 15:26:14 -0300

    corrigi a mensagem do teste e tirei o txt teste

--- mqtt_python_publisher.py
+++ mqtt_python_publisher.py
@@ -2,6 +2,6 @@ import paho.mqtt.client as mqtt
 
 client = mqtt.Client()
 client.connect("mqtt.eclipseprojects.io")
-client.publish("lab/temperatura", "cuca beludo")
+client.publish("lab/temperatura", "teste")
 print("Mensagem enviada!")
 client.disconnect()
